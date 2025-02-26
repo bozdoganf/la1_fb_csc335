bc0ac98de668f86390072e23ec00238f79fc7216
Behruz <91871713+DevBehruz@users.noreply.github.com>
2025-02-25 23:47:03 -0700

    Update MusicStore.java
    Created instance variable for albums and constructor method. Need to implement loadAlbums and parsingFileNames.

--- MusicStore.java
+++ MusicStore.java
@@ -1,5 +1,40 @@
-package la1;
+package la1
+
+import java.util.*;
+import java.io.*;
+
 
 public class MusicStore {
+	
+	/* INSTANCE VARIABLES */
+    private HashMap<String, Object> albums;
+
+	/* CONSTRUCTORS */
+	public MusicStore(String albumsFilePath) {
+		 albums = new HashMap<>();
+	     loadAlbums(albumsFilePath);
+	}
+	
+	private void loadAlbums(String albumsFilePath) {
+        // ...
+    }
+
+    private void loadAlbum(String fileName) {
+        // ...
+    }
+
+
+	
+	/* METHODS */
+
+    public HashMap<String, Object> getAlbum(String title) {
+        for (HashMap<String, Object> album : albums) {
+            if (album.get("title").equals(title)) {
+                return album;
+            }
+        }
+        return null;
+    }
+
 
 }
